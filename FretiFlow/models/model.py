a31944b4284fec860d2c47bba2fc0a8af165cb3e
Steven Borik <SBD2RP@hermanmiller.com>
2023-06-05 10:59:15 -0400

    model update input shape

--- FretiFlow/models/model.py
+++ FretiFlow/models/model.py
@@ -2,16 +2,21 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 
+# Define the network architecture
 model = Sequential()
-model.add(LSTM(units=128, input_shape=(input_shape, 1)))  # input_shape should match the input data shape
-model.add(Dense(units=num_classes, activation='softmax'))  # num_classes is the number of output classes
+model.add(LSTM(units=128, input_shape=(1, 11)))  # Adjust input_shape based on your data
+model.add(Dense(units=num_classes, activation='softmax'))
 
+# Compile the model
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+# Train the model
 model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(X_test, y_test))
 
+# Evaluate the model
 score = model.evaluate(X_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
+# Make predictions
 predictions = model.predict(X_test)
