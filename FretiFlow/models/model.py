1b2429bfd4125e2489cded877e8a70c516ec6372
Steven Borik <SBD2RP@hermanmiller.com>
2023-06-06 13:16:03 -0400

    data loading logic

--- FretiFlow/models/model.py
+++ FretiFlow/models/model.py
@@ -1,10 +1,27 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
+import pandas as pd
+import numpy as np
+
+# Load the training and testing data into Pandas DataFrames
+train_data = pd.read_csv('C:\\Users\\SBD2RP\\OneDrive - MillerKnoll\\installs\\Desktop\\output\\train_data_v2.csv')
+test_data = pd.read_csv('C:\\Users\\SBD2RP\\OneDrive - MillerKnoll\\installs\\Desktop\\output\\test_data_v2.csv')
+
+# Extract the input features and target labels
+X_train = train_data.iloc[:, :-1].values
+y_train = train_data.iloc[:, -1].values
+
+X_test = test_data.iloc[:, :-1].values
+y_test = test_data.iloc[:, -1].values
+
+# Reshape the input features for the LSTM model
+X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
+X_test = np.reshape(X_test, (X_test.shape[0], 1, X_test.shape[1]))
 
 # Define the network architecture
 model = Sequential()
-model.add(LSTM(units=128, input_shape=(1, 11)))  # Adjust input_shape based on your data
+model.add(LSTM(units=128, input_shape=(1, X_train.shape[2])))
 model.add(Dense(units=num_classes, activation='softmax'))
 
 # Compile the model
