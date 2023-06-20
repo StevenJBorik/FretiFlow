9688fcfc83170216df3b4cf21bc298e33fd067a6
Steven Borik <SBD2RP@hermanmiller.com>
2023-06-19 22:17:15 -0400

    LTSM to MLP for non sequential analysis

--- FretiFlow/models/model.py
+++ FretiFlow/models/model.py
@@ -1,10 +1,10 @@
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Input
-from tensorflow.keras.models import Model
 import pandas as pd
 import numpy as np
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Concatenate, Input
+from tensorflow.keras.models import Model
+from sklearn.metrics import precision_score, recall_score
+
 
 # Load the training and testing data into Pandas DataFrames
 train_data = pd.read_csv('C:\\Users\\SBD2RP\\OneDrive - MillerKnoll\\installs\\Desktop\\output\\modified_train_data.csv')
@@ -18,9 +18,9 @@ test_data['input'] = test_data['input'].apply(lambda x: np.array(x.split(',')).a
 
 # Filter out empty arrays
 train_data = train_data[train_data['song.sections_start'].apply(len) > 0]
-train_data['label'] = train_data['label'].apply(lambda x: np.array(x.split(',')).astype(float))
+train_data['label'] = train_data['label'].apply(lambda x: np.array(x.split(',')).astype(float) if isinstance(x, str) else np.array([]))
 test_data = test_data[test_data['song.sections_start'].apply(len) > 0]
-test_data['label'] = test_data['label'].apply(lambda x: np.array(x.split(',')).astype(float))
+test_data['label'] = test_data['label'].apply(lambda x: np.array(x.split(',')).astype(float) if isinstance(x, str) else np.array([]))
 
 # Convert the lists to a list of arrays
 X_train_sections = train_data['song.sections_start'].to_list()
@@ -48,30 +48,32 @@ X_train_input = np.array(X_train_input)
 X_test_sections = np.array(X_test_sections)
 X_test_input = np.array(X_test_input)
 
-# Reshape the input features for the LSTM model
-X_train_sections = np.reshape(X_train_sections, (X_train_sections.shape[0], 1, X_train_sections.shape[1]))
-X_train_input = np.reshape(X_train_input, (X_train_input.shape[0], 1, X_train_input.shape[1]))
-X_test_sections = np.reshape(X_test_sections, (X_test_sections.shape[0], 1, X_test_sections.shape[1]))
-X_test_input = np.reshape(X_test_input, (X_test_input.shape[0], 1, X_test_input.shape[1]))
-
 # Define the network architecture
-input_sections = Input(shape=(1, X_train_sections.shape[2]))
-input_input = Input(shape=(1, X_train_input.shape[2]))
+input_sections = Input(shape=(X_train_sections.shape[1],))
+input_input = Input(shape=(X_train_input.shape[1],))
 
-sections_lstm = LSTM(units=128)(input_sections)
-input_lstm = LSTM(units=128)(input_input)
-
-concatenated = tf.keras.layers.concatenate([sections_lstm, input_lstm])
-
-output = Dense(units=max_length, activation='sigmoid')(concatenated)
+concatenated = Concatenate()([input_sections, input_input])
+dense1 = Dense(units=64, activation='relu')(concatenated)
+dense2 = Dense(units=64, activation='relu')(dense1)
+output = Dense(units=max_length, activation='sigmoid')(dense2)
 
 model = Model(inputs=[input_sections, input_input], outputs=output)
 
 # Compile the model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+# Define precision and recall metrics
+def precision(y_true, y_pred):
+    y_pred_binary = tf.round(y_pred)
+    return precision_score(y_true, y_pred_binary, average='micro')
+
+def recall(y_true, y_pred):
+    y_pred_binary = tf.round(y_pred)
+    return recall_score(y_true, y_pred_binary, average='micro')
+
 # Train the model
-model.fit([X_train_sections, X_train_input], y_train, batch_size=64, epochs=350, validation_data=([X_test_sections, X_test_input], y_test))
+model.fit([X_train_sections, X_train_input], y_train, batch_size=64, epochs=300, validation_data=([X_test_sections, X_test_input], y_test),
+          callbacks=[tf.keras.callbacks.LambdaCallback(on_epoch_end=lambda epoch, logs: print(f'Precision: {precision(y_test, model.predict([X_test_sections, X_test_input])):.4f}, Recall: {recall(y_test, model.predict([X_test_sections, X_test_input])):.4f}'))])
 
 # Evaluate the model
 score = model.evaluate([X_test_sections, X_test_input], y_test, verbose=0)
